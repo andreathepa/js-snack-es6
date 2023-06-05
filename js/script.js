dba7df92c39f14381b65d511539803e07cb3c0ac
andreathepa <andreadepa32@gmail.com>
2023-06-05 23:37:32 +0200

    snack 4 part random numbers

--- js/script.js
+++ js/script.js
@@ -64,3 +64,37 @@ for (let i = 1; i < bici_corsa.length; i++) {
 
 console.log(`La bici più leggera è ${bici_leggera.nome} con un peso di ${bici_leggera.peso} kg.`);
 
+//Snack 4
+
+// Creare un array di oggetti di squadre di calcio.
+
+const teams = [
+    {nome: 'Perugia', punti: 0, falli: 0},
+    {nome: 'Modena', punti: 0, falli: 0},
+    {nome: 'Taranto', punti: 0, falli: 0},
+    {nome: 'Pescara', punti: 0, falli: 0},
+    {nome: 'Vercelli', punti: 0, falli: 0}
+];
+
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+
+function generate_number(min,max){
+    for(let i=0; i<teams.length; i++){
+
+        teams[i].punti = Math.floor(Math.random() * (max - min + 1) + min);
+
+        teams[i].falli = Math.floor(Math.random() * (max - min + 1) + min);
+
+    }
+  
+}
+
+generate_number(0,100)
+
+console.log(teams)
+
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
